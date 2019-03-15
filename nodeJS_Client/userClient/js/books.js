5628795c2c48b6f1efda35a9eaa84cbcce7a9742
AnasMuhammed2020 <mr_seleman@icloud.com>
2019-03-15 16:35:35 +0200

    adding books.js

---
+++ nodeJS_Client/userClient/js/books.js
@@ -0,0 +1,28 @@
+let books_div=document.getElementsByClassName("thumbnails")[0]
+let book_div=document.getElementById("book_div")
+window.addEventListener("load", (evt) => {
+  listBooks();
+})
+function listBooks() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let response = JSON.parse(this.response)
+        response.forEach(book => {
+          let div= document.createElement("div")
+          books_div.appendChild(div)
+          div.innerHTML=book_div.innerHTML
+          div.setAttribute("id",book._id)
+          div.setAttribute("class","box") 
+          div.getElementsByTagName("h3")[0].innerText=book.name
+          div.getElementsByTagName("h3")[1].innerText=book.author_id.first_name
+          div.getElementsByTagName("h3")[0].setAttribute("id", book._id)
+          div.style.display=true
+// add event listeners to send you to author page
+        });
+      }
+    };
+    xhttp.open("GET", "http://127.0.0.1:5000/books");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send()
+  };
