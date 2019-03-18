97d1e79c72dcada809868e07d2dd8070af0ca581
mahmoud <mahmoudalaa25492@gmail.com>
2019-03-18 16:15:09 -0400

    adding rating functionality to front end

--- nodeJS_Client/userClient/js/home.js
+++ nodeJS_Client/userClient/js/home.js
@@ -16,32 +16,32 @@ function listBooks() {
             let response = JSON.parse(this.response)
             let inc = 0
             document.getElementById("username").innerHTML = response.books.name.first_name + " " + response.books.name.last_name
-            
+
             response.books.books.forEach((book) => {
-                
+
                 let div = document.createElement("div")
                 //rate(div)
                 //////all section/////////////////
                 books_div.appendChild(div)
-                hyberBook(div,book,response,inc)
-                
+                hyberBook(div, book, response, inc)
+
                 //////read section/////////////////
                 if (book.status == "Read") {
                     let div = document.createElement("div")
                     books_read_div.appendChild(div)
-                    hyberBook(div,book,response,inc)
+                    hyberBook(div, book, response, inc)
                 }
                 //////reading section/////////////////
                 if (book.status == "Reading") {
                     let div = document.createElement("div")
                     books_reading_div.appendChild(div)
-                    hyberBook(div,book,response,inc)
+                    hyberBook(div, book, response, inc)
                 }
                 //////want to read section/////////////////
                 if (book.status == "Want to read") {
                     let div = document.createElement("div")
                     books_want_div.appendChild(div)
-                    hyberBook(div,book,response,inc)
+                    hyberBook(div, book, response, inc)
                 }
                 // let div =document.getElementsByClassName("thumbnails")[0]
                 // rate(div)
@@ -83,36 +83,43 @@ function listBooks() {
     xhttp.send()
 };
 
-function hyberBook(div,book,response,inc){
+function hyberBook(div, book, response, inc) {
     div.innerHTML = book_div.innerHTML
     div.setAttribute("id", book.book_id._id)
     div.setAttribute("class", "box")
     div.getElementsByTagName("h3")[0].innerText = book.book_id.name
     div.getElementsByTagName("button")[1].innerText = response.authors[inc].first_name
     div.style.display = true
-    let book_button=div.getElementsByTagName('button')[0]
+    let book_button = div.getElementsByTagName('button')[0]
     book_button.setAttribute('id', book.book_id._id)
-    let author_button=div.getElementsByTagName('button')[1]
+    let author_button = div.getElementsByTagName('button')[1]
     author_button.setAttribute('id', book.book_id.author_id)
-    div.getElementsByTagName("span")[0].innerText=book.status
+    div.getElementsByTagName("span")[0].innerText = book.status
     ////add listener on selection from menue
-    let liTags=div.getElementsByTagName("li")
-    for (let i=0;i<3;i++){
-        liTags[i].addEventListener("click",function(event){
-            shelve(this.innerText,book.book_id._id)
+    let liTags = div.getElementsByTagName("li")
+    for (let i = 0; i < 3; i++) {
+        liTags[i].addEventListener("click", function (event) {
+            shelve(this.innerText, book.book_id._id)
         })
     }
     ////add listener on the click on book button
-    book_button.addEventListener("click",function(){
-        localStorage.setItem("bookId",this.id)
-        window.location.href='./bookPage.html'
+    book_button.addEventListener("click", function () {
+        localStorage.setItem("bookId", this.id)
+        window.location.href = './bookPage.html'
     })
     ////add listener on the click on author button
-    author_button.addEventListener("click",function(){
-        localStorage.setItem("authorId",this.id)
-        window.location.href='./authorPage.html'
+    author_button.addEventListener("click", function () {
+        localStorage.setItem("authorId", this.id)
+        window.location.href = './authorPage.html'
     })
-    let bookImage=div.getElementsByTagName("img")[0]
-    getImage(book.book_id.picture,bookImage)
+    // Avg rating
+    let userstars = div.getElementsByClassName('rate')[0].children
+    for (let i = 0; i < Math.round(book.user_rating); i++) {
+      userstars[i].src = './images/goldenstar.png'
+    }
+    ///rate
+    rate(div)
+    let bookImage = div.getElementsByTagName("img")[0]
+    getImage(book.book_id.picture, bookImage)
 
 }
